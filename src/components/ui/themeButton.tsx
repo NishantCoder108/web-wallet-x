dbe19b8fdb0a79b8615f3fe4dbdc96421e2c44c0
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-08-30 02:17:20 +0530

    Implement Theme

---
+++ src/components/ui/themeButton.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+
+    const isDarkMode =
+        theme === "dark" ||
+        (theme === "system" &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+    return (
+        <div className="flex items-center gap-2">
+            <Sun
+                className={`h-5 w-5 ${
+                    isDarkMode ? "text-primary/50" : "text-primary"
+                }`}
+            />
+            <Switch
+                checked={isDarkMode}
+                onCheckedChange={(checked) =>
+                    setTheme(checked ? "dark" : "light")
+                }
+            />
+            <Moon
+                className={`h-5 w-5 ${
+                    isDarkMode ? "text-primary" : "text-primary/50"
+                }`}
+            />
+        </div>
+    );
+}
