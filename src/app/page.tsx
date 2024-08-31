13e196eb6a61dc7245694d878391e4b368f06a86
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-08-31 12:15:41 +0530

    ♻️  Removed Unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import GenerateMnemonic from "@/components/GenerateMnemonic";
-import GenerateWallet from "@/components/GenerateWallet";
-import Navbar from "@/components/Navbar";
 import WalletManager from "@/components/WalletManager";
 
 export default function Home() {
     return (
         <main className="min-h-[70vh]">
-            {/* <GenerateMnemonic /> */}
             <WalletManager />
-            {/* <GenerateWallet /> */}
         </main>
     );
-}
-
-{
-    /* <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]"> */
 }
