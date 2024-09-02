77f1cf12bda31e780092c8ca4ea231edf14ce06d
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-09-02 12:13:45 +0530

    For getting balance, utils implement

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,9 @@ export const getLocalWallet = () => {
     const parsedStorage = storage ? JSON.parse(storage) : null;
 
     return parsedStorage;
+};
+
+export const copyToClipboard = (secretPhrase: string) => {
+    navigator.clipboard.writeText(secretPhrase);
+    toast.success("Copied to clipboard!");
 };
---
+++ src/lib/wallet/getBalance.ts
@@ -0,0 +1,17 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+
+const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || "";
+
+export async function getSolanaBalance(address: string): Promise<number> {
+    try {
+        const connection = new Connection(RPC_URL);
+        const publicKey = new PublicKey(address);
+        const balance = await connection.getBalance(publicKey);
+
+        // Convert lamports to SOL
+        return balance / 1e9;
+    } catch (error) {
+        console.error("Error fetching balance:", error);
+        return 0;
+    }
+}
